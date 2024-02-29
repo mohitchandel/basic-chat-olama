56a92ca4da29509453c995db3b59171ee90bcd17
Mohit Chandel <devmohit93@gmail.com>
2024-02-29 16:03:57 +0530

    some changes

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ headers = {
 data = {
     "model": "mistral",
     "stream": False,
-    "prompt":"why is the sky blue"
+    "prompt":"How to change my discord profile?"
 }
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
 
 if response.status_code == 200:
-    print(response.text)
+    response_text = response.text
+    data = json.loads(response_text)
+    actual_response = data["response"]
+    print(actual_response)
 else:
     print("Error",response.status_code,response.text)
